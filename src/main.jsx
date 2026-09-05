cab705f872441e10639823918848736d948bd262
7-Azhar <ah3709405@gmail.com>
2026-09-05 19:06:38 +0530

    Analytics for the website integration

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 import './index.css'
 import App from './App.jsx'
 
@@ -8,6 +10,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HelmetProvider>
       <App />
+      <SpeedInsights />
     </HelmetProvider>
   </StrictMode>
 )
